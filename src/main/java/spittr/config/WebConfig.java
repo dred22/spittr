fd698038cd3566d66e86c3e823f82c74516c5e24
magomed <magomed@linxo.com>
2018-12-11 15:28:43 +0100

    internationalization messages added

--- src/main/java/spittr/config/WebConfig.java
+++ src/main/java/spittr/config/WebConfig.java
@@ -1,8 +1,10 @@
 package spittr.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -29,4 +31,14 @@ public class WebConfig
             DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource =
+                new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setCacheSeconds(10);
+        return messageSource;
+    }
+
 }
