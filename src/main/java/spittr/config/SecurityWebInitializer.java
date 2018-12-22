ee65f26895ce65a33b4ecac2d788239a234e3e2a
magomed <magomed@linxo.com>
2018-12-22 17:00:28 +0100

    spring mvc web security added, first step

---
+++ src/main/java/spittr/config/SecurityWebInitializer.java
@@ -0,0 +1,10 @@
+package spittr.config;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+public class SecurityWebInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+
+}
