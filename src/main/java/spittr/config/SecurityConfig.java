7dec5d347b5cee8f10db24c1c252954da58fa04a
magomed <magomed@linxo.com>
2018-12-24 12:16:14 +0100

    UserDetailsService lambda

--- src/main/java/spittr/config/SecurityConfig.java
+++ src/main/java/spittr/config/SecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import spittr.data.models.Spitter;
 import spittr.services.SpitterRepository;
@@ -30,25 +28,18 @@ public class SecurityConfig
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
         auth
-                .userDetailsService(new UserDetailsService() {
+                .userDetailsService((username) -> {
+                    Spitter spitter = spitterRepository.findByUsername(username);
+                    if (spitter != null) {
+                        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+                        authorities.add(new SimpleGrantedAuthority("ROLE_SPITTER"));
 
-                    @Override
-                    public UserDetails loadUserByUsername(String username)
-                            throws UsernameNotFoundException {
-                        Spitter spitter = spitterRepository.findByUsername(username);
-                        if (spitter != null) {
-                            List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-                            authorities.add(new SimpleGrantedAuthority("ROLE_SPITTER"));
-
-                            return new User(
-                                    spitter.getUsername(),
-                                    spitter.getPassword(),
-                                    authorities);
-                        }
-
-                        throw new UsernameNotFoundException(
-                                "User '" + username + "' not found.");
+                        return new User(
+                                spitter.getUsername(),
+                                spitter.getPassword(),
+                                authorities);
                     }
+                    throw new UsernameNotFoundException("User '" + username + "' not found.");
                 });
     }
 
