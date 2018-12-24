6fa14a6038b8cfb83769c27d727ca1946865de20
magomed <magomed@linxo.com>
2018-12-24 14:53:16 +0100

    findSpittles mocked data added

--- src/main/java/spittr/services/impl/SpittleRepositoryImpl.java
+++ src/main/java/spittr/services/impl/SpittleRepositoryImpl.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Repository;
 import spittr.data.models.Spittle;
 import spittr.services.SpittleRepository;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public class SpittleRepositoryImpl implements SpittleRepository {
     @Override
     public List<Spittle> findSpittles(long max, int count) {
-        return null;
+        return Arrays.asList(new Spittle("Maga", new Date()), new Spittle("Istre", new Date()));
     }
 
     @Override
