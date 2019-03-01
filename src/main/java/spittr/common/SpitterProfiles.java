11d2c6db0bd033ecf9020e8a3d5083fcba57b926
magomed <magomed@linxo.com>
2019-03-01 18:47:47 +0100

    maven profile

--- src/main/java/spittr/common/SpitterProfiles.java
+++ src/main/java/spittr/common/SpitterProfiles.java
@@ -2,5 +2,5 @@ package spittr.common;
 
 public final class SpitterProfiles {
     public final static String HIBERNATE = "hibernate";
-    public final static String NOT_HIBERNATE = "!hibernate";
+    public final static String IN_MEMORY = "inmemory";
 }
